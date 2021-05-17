e40058752a0b0f5c223d4be72f26da149d114774
DESKTOP-M0OCTGD\jhbyeon <juhan211@naver.com>
2021-05-17 21:54:17 +0900

    useState 사용하기: useState는 import하여 사용하고 0으로 초기화후에 set함수를 사용해서 +,- 한

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Hello from './Hello';
-import Wrapper from './Wrapper'
+import Wrapper from './Wrapper';
+import Counter from './Counter';
+
 function App() {
   return (
+    <>
     <Wrapper>
-      <Hello name="react" color="red"/>
-      <Hello />
-      <Hello />
-      <Hello />
+      <Hello name="react" color="red" isSpecial={true}/>
+      <Hello color="pink" />
     </Wrapper>
-  );
+    <Counter />
+   </>
+  )
 }
 
 export default App;
--- src/Hello.js
+++ src/Hello.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-function Hello({ color, name }) {
-  return <div style={{color}}>안녕하세요 { name }</div>
+function Hello({ color, name, isSpecial }) {
+  return (
+    <div style={{ color }}>
+      { isSpecial ? <b>*</b> : null }
+      안녕하세요 {name}
+    </div>
+  );
 }
 
 Hello.defaultProps = {
-    name:'no name',
-    color : 'blue'
+  name: '이름없음'
 }
 
 export default Hello;
