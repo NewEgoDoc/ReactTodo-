dfe1029af9e518ef1db615c1c99c65e37711cb74
DESKTOP-M0OCTGD\jhbyeon <juhan211@naver.com>
2021-05-17 20:56:47 +0900

    defaultProps 를 사용해 지정되지 않은 tag에 속성과속성값 지정

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import Hello from './Hello';
 
 function App() {
   return (
+    <>
     <Hello name="react" color="red"/>
+    <Hello />
+    <Hello />
+    <Hello />
+    
+    </>
   );
 }
 
--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function Hello({ color, name }) {
-  return <div style={{color}}>안녕하세요 {name}</div>
+  return <div style={{color}}>안녕하세요 { name }</div>
+}
+
+Hello.defaultProps = {
+    name:'no name',
+    color : 'blue'
 }
 
 export default Hello;
