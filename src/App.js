67580b770bd5847ec1bbce89a987ebe3ed3f8a65
DESKTOP-M0OCTGD\jhbyeon <juhan211@naver.com>
2021-05-19 20:11:30 +0900

    배열 항목 추가 수정 삭제

--- src/App.js
+++ src/App.js
@@ -1,11 +1,80 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
 import InputStream from './InputSample';
 import UserList from './UserList'
+import CreateUser from './CreateUser'
+
 
 function App() {
+
+  const [inputs, setInputs] = useState({
+    username:'',
+    email: ''
+  })
+
+  const {username , email} = inputs;
+  const onChange = e =>{
+    const {name,value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    })
+  }
+  const [users, setUsers] = useState([
+    {
+        id: 1,
+        username: 'james',
+        email: 'james@naver.com',
+        active: true
+    },
+    {
+        id: 2,
+        username: 'john',
+        email: 'john@naver.com',
+        active: false
+    },
+    {
+        id: 3,
+        username: 'kate',
+        email: 'kate@naver.com',
+        active: false
+    }
+  ])
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+
+    //setUsers([...users,user]);
+    setUsers(users.concat(user))  
+    setInputs({
+      username:'',
+      email:''
+    })
+    nextId.current+=1;
+  }
+
+  const onRemove = id => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users.filter(user => user.id !== id));
+  };
+
+  const onToggle = id => {
+    setUsers(
+      users.map(user =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
   return (
     <>
     <Wrapper>
@@ -14,7 +83,13 @@ function App() {
     </Wrapper>
     <Counter />
     <InputStream />
-    <UserList/>
+    <CreateUser 
+      username = { username }
+      email = { email }
+      onChange = { onChange }
+      onCreate = { onCreate }
+    />
+    <UserList users={ users } onRemove = { onRemove } onToggle={onToggle}/>
    </>
   )
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,36 +1,30 @@
 import React from 'react'
 
-function User({user}){
+function User({user,onRemove,onToggle}){
     return(
         <div>
-            <b>{user.username}</b><span>({user.email})</span>
+            <b
+                style={{
+                    cursor:'pointer',
+                    color: user.active? 'green': 'black'
+                }}
+                onClick={() => onToggle(user.id)}
+            >
+                {user.username}
+            </b>
+            <span>({user.email})</span>
+            <button onClick={() => onRemove(user.id)}>삭제</button>
         </div>
     );
 }
 
 
-function UserList(){
-    const users = [
-        {
-            id: 1,
-            username: 'james',
-            email: 'james@naver.com'
-        },
-        {
-            id: 2,
-            username: 'john',
-            email: 'john@naver.com'
-        },
-        {
-            id: 3,
-            username: 'kate',
-            email: 'kate@naver.com'
-        }
-    ]
+function UserList({users, onRemove, onToggle }){
+    
     return (
         <div>
             {users.map(user => (
-                <User user={user}/>
+                <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}/>
             ))}
         </div>
     );
