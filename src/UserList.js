e0d4056274f9d4ebd4fd87475dcc9b9f671a3192
DESKTOP-M0OCTGD\jhbyeon <juhan211@naver.com>
2021-05-20 19:39:15 +0900

    useEffect읭 사용과 연산,useMemo 를 사용하여 연산한 값 재사용

--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function User({user,onRemove,onToggle}){
+
+    useEffect(() => {
+        console.log(user);
+      });
+
     return(
         <div>
             <b
@@ -12,6 +17,7 @@ function User({user,onRemove,onToggle}){
             >
                 {user.username}
             </b>
+            &nbsp;
             <span>({user.email})</span>
             <button onClick={() => onRemove(user.id)}>삭제</button>
         </div>
