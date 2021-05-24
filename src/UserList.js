de869e2f1a3960c9e81675dc44224530e6b32cd8
DESKTOP-M0OCTGD\jhbyeon <juhan211@naver.com>
2021-05-24 20:07:26 +0900

    Context API 를 사용한 전역 값 관리

--- src/UserList.js
+++ src/UserList.js
@@ -1,39 +1,43 @@
-import React, { useEffect } from 'react'
+import React, { useContext } from 'react';
+import { UserDispatch } from './App';
 
-function User({user,onRemove,onToggle}){
+const User = React.memo(function User({ user }) {
+  const dispatch = useContext(UserDispatch);
 
-    useEffect(() => {
-        console.log(user);
-      });
+  return (
+    <div>
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black'
+        }}
+        onClick={() => {
+          dispatch({ type: 'TOGGLE_USER', id: user.id });
+        }}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
+      <button
+        onClick={() => {
+          dispatch({ type: 'REMOVE_USER', id: user.id });
+        }}
+      >
+        삭제
+      </button>
+    </div>
+  );
+});
 
-    return(
-        <div>
-            <b
-                style={{
-                    cursor:'pointer',
-                    color: user.active? 'green': 'black'
-                }}
-                onClick={() => onToggle(user.id)}
-            >
-                {user.username}
-            </b>
-            &nbsp;
-            <span>({user.email})</span>
-            <button onClick={() => onRemove(user.id)}>삭제</button>
-        </div>
-    );
-}
-
-
-function UserList({users, onRemove, onToggle }){
-    
-    return (
-        <div>
-            {users.map(user => (
-                <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}/>
-            ))}
-        </div>
-    );
+function UserList({ users }) {
+  return (
+    <div>
+      {users.map(user => (
+        <User user={user} key={user.id} />
+      ))}
+    </div>
+  );
 }
 
 export default React.memo(UserList);
